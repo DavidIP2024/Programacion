c5097f6e09cb8121355aef8554a74cd376017ada
David Iglesias <diglesiasperez@danielcastelao.org>
2024-11-14 12:02:37 +0100

    o

---
+++ Boletin1/src/Ejercicio1.java
@@ -0,0 +1,28 @@
+public class Ejercicio1 {
+    /**
+     * Ejercicio 1
+     * **/
+    public static void main(String[] args) {
+        // Primera operacion
+        int resultado1 = ((3+2) % 2 -15) / 2 * 5;
+        System.out.println(resultado1);
+        // Segunda operacion
+        int resultado2 = (6 + 6 / 7) + 35 / 2 -8 * 5 / 4 * 2;
+        System.out.println(resultado2 );
+        // Tercera operacion
+        int resultado3 = 3 + 6 * 14 % 3;
+        System.out.println(resultado3);
+        // Cuarta operacion
+        int resultado4 = 8 + 7 * 3 + 4 * 6 /2 % 4;
+        System.out.println(resultado4);
+        // Quinta operacion
+        int resultado5 = (27%4)+(15/4);
+        System.out.println(resultado5);
+        // Sexta operacion
+        int resultado6 = 37 / (4*4) - 2;
+        System.out.println(resultado6);
+        //Octava operacion
+        int resultado8 = ((7*3-4*4) * 2)/4 *2;
+        System.out.println(resultado6);
+    }
+}
